0de2e12f8a6a178e7f41e090ab3da4b227c0cccb
Juantorresma <31540688+Juantorresma@users.noreply.github.com>
2020-02-20 17:37:30 -0600

    Update asm-analytics.sh

--- labs/01/asm-analytics.sh
+++ labs/01/asm-analytics.sh
@@ -1,39 +1,39 @@
 # Homework 01
 # A01227885
 # Juan Torres
+instr_regex='[0-9a-f]{4}:\s+([0-9a-f]{2} )+\s+(\w+)'
+func_regex='^([0-9a-f]+) <(.*)>:$'
 
-import sys
+declare -A instructions
+declare -A functions
 
-def main():
-	print("Hi, this is the output of the analysis: ")
-	funcs = []
-	scriptRoute =  sys.argv[1]
-	command = {}
-  
-	with open(scriptRoute) as file:
-		for line in file:
-			if line[0]=='0':
-				words =  line.split(" ")
-				funcs.append(words)
-			else:
-				words = line.split("\t")
-				if len(words)>2:
-					commands = words[2].split(" ")
-					if len(commands) >0: 
-						if commands[0] in command:
-							command[commands[0]] = command[commands[0]]+1
-						else:
-							command[commands[0]] = 1
+while read line
+do
+        if [[ $line =~ $func_regex ]]; then
+                if [[ ! -v functions[${BASH_REMATCH[2]}] ]]; then
+                        echo "${BASH_REMATCH[1]}"
+                        functions[${BASH_REMATCH[2]}]=${BASH_REMATCH[1]}
+                fi
+        elif [[ $line =~ $instr_regex ]]; then
+                if [[ -v instructions[${BASH_REMATCH[2]}] ]]; then
+                        (( instructions[${BASH_REMATCH[2]}]++ ))
+                else
+                        instructions[${BASH_REMATCH[2]}]=1
+                fi
+        fi
+done < $1
 
-	print("You have {0} kind of instructions in this obj. file:".format(len(command)))
-	for comm in command:
-		print("  {0}\t: Executed {1} times ".format(comm, command[comm]))
-	print("You have {0} functions:".format(len(funcs)))
-	for x in  funcs:
-		print("  {0}".format( x[1][1:-3] ), end='' )
-		space =(24-len(x[1][1:-3])) * ' '
-		print(space, end='')
-		print(": Located at {0} addr".format(x[0][-4:] ))
+echo 'Hi, this is the output of the analysis:'
+echo '    You have' ${#instructions[@]} 'kind(s) of instructions in this object file:'
 
-if __name__ == '__main__':
-	main()
+for key in "${!instructions[@]}"
+do
+        printf '        %-9s: Executed %d times\n' $key ${instructions[$key]}
+done
+
+echo '    You have' ${#functions[@]} 'functions:'
+
+for key in "${!functions[@]}"
+do
+        printf '        %-25s: Located at %s addr\n' $key ${functions[$key]}
+done
