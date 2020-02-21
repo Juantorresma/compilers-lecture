8e703ed66f00417fc70020e12e93b687f9b0ec4b
Juantorresma <31540688+Juantorresma@users.noreply.github.com>
2020-02-21 10:56:17 -0600

    Update asm-analytics.sh

--- labs/01/asm-analytics.sh
+++ labs/01/asm-analytics.sh
@@ -2,43 +2,37 @@
 # A01227885
 # Juan Torres
 
-#Declaring the func and instr regular expressions
-instr_regex='[0-9a-f]{4}:\s+([0-9a-f]{2} )+\s+(\w+)'
-func_regex='^([0-9a-f]+) <(.*)>:$'
+import sys
+import re
 
-#where we are going to put our instructinons and functions
-declare -A instr
-declare -A func
+#Declaring the func, instr and addr regular expressions
+instr_regex = re.compile(r'<.+>:')
+func_regex = re.compile(r' \t[a-z]+[0-9]*[a-z]* [a-z]+[0-9]*[a-z]*\s' '| \t[a-z]+[0-9]*[a-z]*\s')
+addr_regex = re.compile(r'[0-9a-fA-F]{16} ')
 
-#we have to read all the lines in our objdump, so we start a while loop to read them
-while read line
-do
-#check if the line belongs to a regular expression of a function or of an instruction
-        if [[ $line =~ $func_regex ]]; then
-                if [[ ! -v func[${BASH_REMATCH[2]}] ]]; then
-                         func[${BASH_REMATCH[2]}]=${BASH_REMATCH[1]}
-                fi
-        elif [[ $line =~ $instr_regex ]]; then
-                if [[ -v instr[${BASH_REMATCH[2]}] ]]; then
-                        (( instr[${BASH_REMATCH[2]}]++ ))
-                else
-                        instr[${BASH_REMATCH[2]}]=1
-                fi
-        fi
-done < $1
+#we have to read all the lines in our objdump, so we create the function asmAnalytics receiving the file
+def asmAnalytics(file):
 
+#we open and read the file into "lines"
+        f=open(file,"r")
+        lines=f.read()
+        instr = [i.strip() for i in instr_regex.findall(lines)]
+        func = [i.strip("<.>:") for i in func_regex.findall(lines)]
+        addr = [i.strip() for i in addr_regex.findall(lines)]
+        
+#we now have to count the number of functions and of the differnet kind of intructions
+        instr_count = len([[x,instructions.count(x)] for x in set(instructions)])
+        func_count=len(func)
+  
 #now we just print what we have
-echo 'Hi, this is the output of the analysis:'
-echo '    You have' ${#instr[@]} 'kind(s) of instructions in this object file:'
+        print("Hi, this is the output of the analysis:","\n\tYou have " , inst_count," kind of instructions in this object file:")
+        for x in inst_count:
+            print("\t\t",x[0], "\t: Executed ", x[1], " times")
+ 
+        print("\tYou have ", func_count, " functions:")
+        for x,y in zip(functions,address):
+                print("\t\t",x,"\t: Located at ",y," addr")
 
-for key in "${!instr[@]}"
-do
-        printf '        %-9s: Executed %d times\n' $key ${instr[$key]}
-done
+        
 
-echo '    You have' ${#func[@]} 'functions:'
-
-for key in "${!func[@]}"
-do
-        printf '        %-25s: Located at %s addr\n' $key ${func[$key]}
-done
+asmAnalytics(sys.argv[1])
