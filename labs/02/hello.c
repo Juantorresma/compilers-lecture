7f2054aee3ca49357d4db6f4af1d5650cb8c0e82
Juantorresma <31540688+Juantorresma@users.noreply.github.com>
2020-03-02 19:53:18 -0600

    Update hello.c

--- labs/02/hello.c
+++ labs/02/hello.c
@@ -1,14 +1,8 @@
-#include <stdio.h>
-
-
-int foo(){
-	//this is my homework :)
-	return 3;
-}
-
-int main(){
-    for (int x = 0; x < 10;x++){
-        printf("hi\n");
+ #include <stdio.h>
+    // coment (this should not be detected
+    int main(){
+        for (int x = 0; x < 10;x++)
+            printf("hi\n);
+        }
+        return 0;
     }
-    return 0;
-}
